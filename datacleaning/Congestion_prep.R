02ac0e37527305cbea7cdb5f09e1354b3ec52c47
dflynn-volpe <daniel.flynn@dot.gov>
2019-06-05 11:30:37 -0400

    Calculating congestion

---
+++ datacleaning/Congestion_prep.R
@@ -0,0 +1,122 @@
+# Prepare congestion values for Air Time models
+# By year, for each airport, calculate number of flights arriving or departing by hour of day, day of week, month of year.
+# Year file will be at most 320 airports x 12 months x 7 days x 24 hours = 645,120 rows. Will have an origin and departure file for each year.
+
+# Setup ----
+# Check to see if working on EC2 instance or local
+if(Sys.getenv('USER')=='rstudio'){
+  codeloc = '~/Wheels_Up/Users/Daniel.Flynn/Documents/git/Wheels_Up'  
+  sharedloc = "~/Data"
+  resultsloc = "~/results"
+} else { 
+  
+  codeloc = ifelse(grepl('Flynn', normalizePath('~/')),
+                   "~/git/Wheels_Up",
+                   "~/GitHub/Wheels_Up")
+  sharedloc = "//vntscex.local/DFS/Projects/PROJ-OR02A2/SDI/BTS_Flight_performance/Data"
+  resultsloc = "//vntscex.local/DFS/Projects/PROJ-OR02A2/SDI/BTS_Flight_performance/Results"
+  
+}
+
+library(tidyverse) # if this fails, run install.packages('tidyverse')
+library(doParallel)
+library(foreach)
+# Congestion prep  ----
+
+# Data will already by subset to airports used by focal carriers (don't include airports not served by at least one focal carriers, each year)
+
+if(!file.exists(file.path(sharedloc, 'ASQP_2015-2018.RData'))){
+  source(file.path(codeloc, 'datacleaning', 'Analysis_prep.R'))
+} else {
+  load(file.path(sharedloc, 'ASQP_2015-2018.RData'))
+}
+
+airports = sort(unique(c(d_crossyear$ORIGIN, d_crossyear$DEST))); cat(length(airports), 'Airports \n')
+# Prepare congestion file if not completed already
+
+if(!file.exists(file.path(sharedloc, 'Congestion_2015-2018.RData'))){
+  
+  # Sequential loop for year
+  for(yr in unique(d_crossyear$YEAR)){
+    # yr = '2015'
+    
+    cat(yr, '. ')
+    dy = d_crossyear %>% 
+      filter(YEAR == yr) 
+    
+    dy_o <- dy %>% 
+      group_by(ORIGIN, MONTH, DAY_OF_WEEK, DEP_TIME_BLK) %>%
+      summarize(N_flights = n())  
+    
+    # Create relative congestion within an airport across year
+    dy_ox <- dy_o %>%
+      group_by(ORIGIN) %>%
+      summarize(N_flights_max = max(N_flights))
+    
+    dy_o <- left_join(dy_o, dy_ox) %>%
+      mutate(Relative_Congestion = N_flights / N_flights_max) %>%
+      select(-N_flights_max)
+    
+    assign(paste0('Congestion_Origin_', yr), dy_o)
+  
+    dy_d <- dy %>% 
+      group_by(DEST, MONTH, DAY_OF_WEEK, ARR_TIME_BLK) %>%
+      summarize(N_flights = n())  
+    
+    # Create relative congestion within an airport across year
+    dy_dx <- dy_d %>%
+      group_by(DEST) %>%
+      summarize(N_flights_max = max(N_flights))
+    
+    dy_d <- left_join(dy_d, dy_dx) %>%
+      mutate(Relative_Congestion = N_flights / N_flights_max) %>%
+      select(-N_flights_max)
+    
+    assign(paste0('Congestion_Destination_', yr), dy_d)
+    
+  }
+  
+  cong_files = ls()[grep('Congestion_', ls())]
+  
+  save(list = cong_files,
+       file = file.path(sharedloc, 'Congestion_2015-2018.RData'))
+} else {
+  load(file.path(sharedloc, 'Congestion_2015-2018.RData'))
+}
+
+# <><><> Some plotting 
+
+NOTRUN = T
+if(NOTRUN){
+  # Probably too much detail. Shows distrubtion shift by time of day at least.
+  ggplot(Congestion_Destination_2015) + 
+    geom_histogram(aes(x = Relative_Congestion, group = as.factor(MONTH), fill = as.factor(MONTH))) + 
+    facet_wrap(~ARR_TIME_BLK) 
+  
+  ggplot(Congestion_Origin_2015) + 
+    geom_histogram(aes(x = Relative_Congestion, group = as.factor(MONTH), fill = as.factor(MONTH))) + 
+    facet_wrap(~DEP_TIME_BLK) 
+  
+  # Too much...
+  ggplot(Congestion_Destination_2015) + 
+    geom_violin(aes(y = Relative_Congestion, 
+                    x = ARR_TIME_BLK)) +
+    facet_wrap(~MONTH) 
+  
+  # Better
+  ggplot(Congestion_Destination_2015) + 
+    geom_boxplot(aes(y = Relative_Congestion, 
+                    x = ARR_TIME_BLK)) 
+
+  ggplot(Congestion_Origin_2015) + 
+    geom_boxplot(aes(y = Relative_Congestion, 
+                     x = DEP_TIME_BLK)) 
+  
+  # Not much variation by dow
+  ggplot(Congestion_Origin_2015) + 
+    geom_boxplot(aes(y = Relative_Congestion, 
+                     x = DEP_TIME_BLK)) +
+    facet_wrap(~DAY_OF_WEEK)
+  
+  
+}
