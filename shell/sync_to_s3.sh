e357722438db75f6f655d50cfcbc97a3443de41b
dflynn-volpe <daniel.flynn@dot.gov>
2019-06-05 14:51:19 -0400

    Fixing sync script from EC2 to S3

--- shell/sync_to_s3.sh
+++ shell/sync_to_s3.sh
@@ -1,7 +1,5 @@
-
-# 1. zip
 sudo zip -r /home/rstudio/results.zip /home/rstudio/results
-# sudo zip -r /home/rstudio/data.zip /home/rstudio/data
+# sudo zip -r /home/rstudio/Data.zip /home/rstudio/Data
 sudo zip -r /home/rstudio/Wheels_Up_Code_Update.zip /home/rstudio/Wheels_Up
 
 # 2. copy to root from rstudio user
@@ -9,10 +7,16 @@ sudo zip -r /home/rstudio/Wheels_Up_Code_Update.zip /home/rstudio/Wheels_Up
 mkdir -p ~/to_s3/
 
 sudo cp -a /home/rstudio/results.zip ~/to_s3/.
-#sudo cp -a /home/rstudio/data.zip ~/to_s3/.
+# sudo cp -a /home/rstudio/data.zip ~/to_s3/.
 sudo cp -a /home/rstudio/Wheels_Up_Code_Update.zip ~/to_s3/.
 
 # 3. copy to bucket
-aws s3 cp ~/to_s3/results.zip s3://volpe-cat01/to_s3/results.zip
-#aws s3 cp ~/to_s3/data.zip s3://volpe-cat01/to_s3/data.zip
-aws s3 cp ~/to_s3/Wheels_Up_Code_Update.zip s3://volpe-cat01/to_s3/Wheels_Up_Code_Update.zip
+
+syncdate=`date +"%Y-%m-%d"`
+
+echo $syncdate
+
+# aws s3 cp ~/to_s3/Data.zip "s3://volpe-cat01/${syncdate}/Data.zip"
+aws s3 cp ~/to_s3/results.zip "s3://volpe-cat01/${syncdate}/results.zip"
+aws s3 cp ~/to_s3/Wheels_Up_Code_Update.zip "s3://volpe-cat01/${syncdate}/Wheels_Up_Code_Update.zip"
+
